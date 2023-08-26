a34f131bb432aa1068c2a8fed66ed1903d2a3e3c
김용민 <dydals3440@gmail.com>
2023-08-26 19:43:06 +0900

    refactor: 6-5-2, 3

--- ch6/6-5-2.js
+++ ch6/6-5-2.js
@@ -3,8 +3,12 @@ export default class Book {
   constructor() {
     this.#reservations = [];
   }
+  // 추가적인 기능이 필요할때 isPriority (boolean을 매개변수로 전달하는 것은 좋지않음, 굳이 해야한다면 true/false로 구분 두개로 나눔!)
+  addReservation(customer, isPriority = true) {
+    this.#reservations.push(customer);
+  }
 
-  addReservation(customer) {
+  addReservation(customer, isPriority = false) {
     this.#reservations.push(customer);
   }
 
--- ch6/6-5-3.js
+++ ch6/6-5-3.js
@@ -1,3 +1,10 @@
 export function inNewEngland(aCustomer) {
   return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
 }
+
+// 함수에서 필요한것은 state 전체 손님의 객체를 받을 필요없음, 왜냐하면 state라는 정보만 있을때는 재사용이 어렵기때문에 정말 필요한 데이터만 받아오는 것이 중요!
+// 호출하는 곳에서 aCustomer.address를 전달!
+// 외부 객체에 대한 의존도를 낮춤!
+export function inNewEngland(state) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(state);
+}
