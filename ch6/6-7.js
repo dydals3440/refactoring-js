bafc85be57363cdaf55e785fc6c094015b56917e
김용민 <dydals3440@gmail.com>
2023-08-26 19:08:18 +0900

    refactor: 6-7 변수명 짓기

--- ch6/6-7.js
+++ ch6/6-7.js
@@ -1,6 +1,17 @@
+// bad
 let a = height * width;
+// good
+let area = width * height;
 
+// bad
 const cpyNm = '애플';
+// good
+const companyName = '애플';
 
+// bad
 let tpHd = '제목없음';
 let result = `<h1>${tpHd}</h1>`;
+
+// good
+let pageTitle = '제목없음';
+let title = `<h1>${title}</h1>`;
